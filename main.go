b15026c77653530d4d659215fd97e4027726d31d
shiinobu <kurogamiaoi1@gmail.com>
2025-04-24 22:59:24 +0700

    test

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 )
 
 func main() {
-    a := app.App{}
-    a.Initialize()
-    if err := a.Run(":" + os.Getenv("SERVER_PORT")); err != nil {
+	a := app.App{}
+	a.Initialize()
+
+	if err := a.Run(":" + os.Getenv("SERVER_PORT")); err != nil {
 		a.ERROR.Fatal("Application failed: ", err)
 	}
 }
