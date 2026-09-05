22c22e78bade005e67ec894f09ba3894abacfe95
Firman Aprilian S. <55623461+shiinobu@users.noreply.github.com>
2026-09-06 01:05:14 +0700

    security: move JWT secret to environment

--- app/config/config.go
+++ app/config/config.go
@@ -1,3 +1,24 @@
 package config
 
-var JWT_KEY = []byte("S5Q)=F],-H97mGvf!A4[+M")
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func Load() error {
+	if err := godotenv.Load(); err != nil {
+		// .env is optional when environment variables are provided by the runtime.
+	}
+
+	if os.Getenv("JWT_KEY") == "" {
+		return fmt.Errorf("JWT_KEY environment variable is required")
+	}
+
+	return nil
+}
+
+func JWTKey() []byte {
+	return []byte(os.Getenv("JWT_KEY"))
+}
